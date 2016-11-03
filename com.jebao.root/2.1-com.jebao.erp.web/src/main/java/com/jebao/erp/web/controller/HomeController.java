fcfd50b3731f5eccbc75f115311695c79b988293
yaozd <2674290996@qq.com>
2016-11-03 15:59:27 +0800

    增加html开发模板--template.html

---
+++ com.jebao.root/2.1-com.jebao.erp.web/src/main/java/com/jebao/erp/web/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.jebao.erp.web.controller;
+
+import com.jebao.erp.web.requestModel.home.IndexForm;
+import com.jebao.erp.web.requestModel.user.UserForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2016/11/2.
+ */
+@Controller
+@RequestMapping("/home/")
+public class HomeController {
+    @RequestMapping("index")
+    public String index(@ModelAttribute("form") IndexForm form) {
+        return "home/index";
+    }
+}
