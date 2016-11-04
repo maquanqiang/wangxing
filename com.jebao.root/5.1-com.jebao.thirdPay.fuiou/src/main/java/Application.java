f03281c9d0c99cab9d762264dd54c3fd896521ed
yaozd <2674290996@qq.com>
2016-11-04 09:56:51 +0800

    删除--第三方富友

--- com.jebao.root/5.1-com.jebao.thirdPay.fuiou/src/main/java/Application.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Administrator on 2016/11/3.
- */
-public class Application {
-    public static void main(String[] args) {}
-}
--- com.jebao.root/5.1-com.jebao.thirdPay.fuiou/src/main/java/com/jebao/thirdPay/fuiou/model/onlineBankRecharge/OnlineBankRechargeResponse.java
+++
@@ -1,7 +0,0 @@
-package com.jebao.thirdPay.fuiou.model.onlineBankRecharge;
-
-/**
- * Created by Administrator on 2016/9/27.
- */
-public class OnlineBankRechargeResponse {
-}
