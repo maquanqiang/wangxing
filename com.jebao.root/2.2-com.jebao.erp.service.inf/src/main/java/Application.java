be6116d1ef7a9e3814ccfd2dfa975e93aa73b5ff
yaozd <2674290996@qq.com>
2016-11-15 14:15:47 +0800

    add -com.jebao.erp.service.impl-com.jebao.erp.service.inf

--- com.jebao.root/2.2-com.jebao.erp.service.inf/src/main/java/Application.java
+++ com.jebao.root/2.2-com.jebao.erp.service.inf/src/main/java/Application.java
@@ -2,4 +2,5 @@
  * Created by Administrator on 2016/11/15.
  */
 public class Application {
+    public static void main(String[] args) {}
 }
--- com.jebao.root/2.2-com.jebao.erp.service.inf/src/main/java/com/jebao/erp/service/inf/ITempTestServiceInf.java
+++ com.jebao.root/2.2-com.jebao.erp.service.inf/src/main/java/com/jebao/erp/service/inf/ITempTestServiceInf.java
@@ -1,7 +1,13 @@
 package com.jebao.erp.service.inf;
 
+import com.jebao.jebaodb.entity.TbTempTest;
+
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/11/15.
  */
-public class ITempTestServiceInf {
+public interface ITempTestServiceInf {
+    int add(TbTempTest data);
+    List<TbTempTest> getList(int pageIndex,int pageSize);
 }
