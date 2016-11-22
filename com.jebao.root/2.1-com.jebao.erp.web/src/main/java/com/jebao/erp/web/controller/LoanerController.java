f4c8741e72af49bc3cfcc6a2fdf5b6e5a8d23606
wangwei <271416914@qq.com>
2016-11-22 10:13:15 +0800

    借款人controller

--- com.jebao.root/2.1-com.jebao.erp.web/src/main/java/com/jebao/erp/web/controller/LoanerController.java
+++ com.jebao.root/2.1-com.jebao.erp.web/src/main/java/com/jebao/erp/web/controller/LoanerController.java
@@ -1,7 +1,18 @@
 package com.jebao.erp.web.controller;
 
+import com.jebao.erp.service.inf.loaner.ILoanerServiceInf;
+import com.jebao.erp.web.requestModel.loaner.SearchForm;
+import com.jebao.jebaodb.entity.loaner.TbLoaner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/11/14.
@@ -9,4 +20,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/loaner/")
 public class LoanerController extends _BaseController {
+    @Autowired
+    private ILoanerServiceInf loanerService;
+
+    @RequestMapping("index")
+    public String index(Model model){
+        List<TbLoaner> list = loanerService.selectLoanerByParamsForPage(null,0,100);
+        model.addAttribute("list",list);
+        return "loaner/index";
+    }
+
+    @RequestMapping("index")
+    public String doSeach(@ModelAttribute("form") SearchForm form,Model model){
+        TbLoaner record = new TbLoaner();
+        String phone = form.getPhone();
+        if(!StringUtils.isEmpty(phone)){
+            record.setlPhone(phone);
+        }
+        int pageSize = loanerService.selectLoanerByParamsForPageCount(record);
+        if(pageSize>0){
+            List<TbLoaner> list = loanerService.selectLoanerByParamsForPage(record,0,pageSize);
+            model.addAttribute("list",list);
+        }
+        return "loaner/index";
+    }
+
+    @RequestMapping(value="/details/{id}",method = RequestMethod.GET)
+    public String details(@PathVariable Long id, Model model){
+        TbLoaner loaner = loanerService.findLoanerById(id);
+        model.addAttribute("loaner",loaner);
+        return "loaner/details";
+    }
 }
---
+++ com.jebao.root/2.1-com.jebao.erp.web/src/main/java/com/jebao/erp/web/requestModel/loaner/SearchForm.java
@@ -0,0 +1,36 @@
+package com.jebao.erp.web.requestModel.loaner;
+
+import com.jebao.erp.web.requestModel.base._BaseForm;
+
+/**
+ * Created by Administrator on 2016/11/22.
+ */
+public class SearchForm extends _BaseForm {
+    private String nickName;
+    private String phone;
+    private String trueName;
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getTrueName() {
+        return trueName;
+    }
+
+    public void setTrueName(String trueName) {
+        this.trueName = trueName;
+    }
+}
