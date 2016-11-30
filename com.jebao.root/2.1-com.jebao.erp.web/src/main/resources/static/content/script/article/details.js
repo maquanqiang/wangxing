a8690c12fb637fc7b58f989f9538a1b2ca1adf0f
wangwei <271416914@qq.com>
2016-11-30 16:48:55 +0800

    文章管理功能添加

--- com.jebao.root/2.1-com.jebao.erp.web/src/main/resources/static/content/script/article/details.js
+++ com.jebao.root/2.1-com.jebao.erp.web/src/main/resources/static/content/script/article/details.js
@@ -1,10 +1,13 @@
 /**
- * Created by Administrator on 2016/11/28.
+ * Created by Administrator on 2016/11/30.
  */
 var model = {
-    risk: {},
+    //文章信息
+    article: {typeId:0},//  typeId:0 初始化下拉框的值，默认选中项
+    //查询条件
     searchObj: {},
-    loanerId: ""
+    //文章类别
+    aTypes: []
 };
 // 创建一个 Vue 实例 (ViewModel),它连接 View 与 Model
 var vm = new Vue({
@@ -13,7 +16,12 @@ var vm = new Vue({
     beforeCreate:function(){
         //初始化本地数据
         model.searchObj = $("#search_form").serializeObject();
-        model.loanerId = $("#order_search_form").find("[name=loanerId]").val();
+        //文章类别
+        $.get("/api/articletype/list",function(response){
+            if (response.success_is_ok){
+                vm.aTypes=response.data;
+            }
+        });
     },
     //初始化远程数据
     created: function () {
@@ -21,17 +29,24 @@ var vm = new Vue({
     },
     mounted:function(){
         this.bindFormValidate();
+        $('.chooseDate').datepicker({
+            format: 'yyyy-mm-dd',
+            weekStart: 1,
+            autoclose: true,
+            todayBtn: 'linked',
+            language: 'cn'
+        });
     },
     //方法，可用于绑定事件或直接调用
     methods: {
-        init: function(){
-            $.get("/api/risk/info", model.searchObj, function (response) {
+        init: function() {
+            $.get("/api/article/details", model.searchObj, function (response) {
                 if (response.success_is_ok) {
                     var data = response.data;
                     if(data != null) {
                         //设置kindEditorContent编辑器
-                        KindEditor.html("#kindEditorContent", data.desc);
-                        vm.risk = data;
+                        KindEditor.html("#kindEditorContent", data.content);
+                        vm.article = data;
                     }
                 }
             });
@@ -40,59 +55,35 @@ var vm = new Vue({
         bindFormValidate:function(){
             $("#order_search_form").bootstrapValidator({
                 fields: {
-                    name: {
-                        message: '项目名称验证失败',
+                    title: {
+                        message: '文章标题验证失败',
                         validators: {
                             notEmpty: {
-                                message: '项目名称不能为空'
+                                message: '文章标题不能为空'
                             }
                         }
                     },
-                    borrowDesc: {
-                        message: '借款描述验证失败',
+                    editDate: {
+                        message: '编辑日期验证失败',
                         validators: {
                             notEmpty: {
-                                message: '借款描述不能为空'
+                                message: '编辑日期不能为空'
                             }
                         }
                     },
-                    fundsPurpose: {
-                        message: '资金用途验证失败',
+                    content: {
+                        message: '文章内容验证失败',
                         validators: {
                             notEmpty: {
-                                message: '资金用途不能为空'
+                                message: '文章内容不能为空'
                             }
                         }
                     },
-                    repayingSource: {
-                        message: '还款来源验证失败',
+                    typeId:{
                         validators: {
-                            notEmpty: {
-                                message: '还款来源不能为空'
-                            }
-                        }
-                    },
-                    mortgageInfo: {
-                        message: '抵押信息验证失败',
-                        validators: {
-                            notEmpty: {
-                                message: '抵押信息不能为空'
-                            }
-                        }
-                    },
-                    personalCredit: {
-                        message: '个人征信验证失败',
-                        validators: {
-                            notEmpty: {
-                                message: '个人征信不能为空'
-                            }
-                        }
-                    },
-                    opinion: {
-                        message: '风控意见验证失败',
-                        validators: {
-                            notEmpty: {
-                                message: '风控意见不能为空'
+                            greaterThan: {
+                                value: 1,
+                                message: '请选择文章类别'
                             }
                         }
                     }
@@ -106,11 +97,14 @@ var vm = new Vue({
             if(!bootstrapValidator.isValid()){
                 return false;
             }
+            console.log("post111");
             var submitModel = $("#order_search_form").serializeObject();
-            $.post("/api/risk/post",submitModel,function(response){
+            console.log(submitModel);
+            $.post("/api/article/post",submitModel,function(response){
+                console.log("post2222");
                 if (response.success_is_ok){
                     layer.msg(response.msg);
-                    var targetUrl="/loaner/risk/index/" + vm.loanerId;
+                    var targetUrl="/article/index";
                     redirectUrl(targetUrl);
                     return;
                 }else{
@@ -121,7 +115,7 @@ var vm = new Vue({
             });
         },
         cancel: function(){
-            var targetUrl="/loaner/risk/index/" + vm.loanerId;
+            var targetUrl="/article/index";
             redirectUrl(targetUrl);
             return;
         }
