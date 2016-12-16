2b5a6e9ff3d2fb81fd02482cc7ed56fa5fbdd90a
yaozd <2674290996@qq.com>
2016-12-16 13:55:27 +0800

    增加文件上传服务API

---
+++ com.jebao.root/5.1-com.jebao.file.web/src/main/java/com/jebao/file/web/utils/constants/Constants.java
@@ -0,0 +1,11 @@
+package com.jebao.file.web.utils.constants;
+
+/**
+ * Created by Administrator on 2016/12/16.
+ */
+public class Constants {
+
+    public static final String FILE_SERVICE_URL=ProjectSetting.getConfigProperty("project.file.service.url");
+
+    public static final String FILE_UPLOAD_DIR=ProjectSetting.getConfigProperty("project.file.upload.dir");
+}
