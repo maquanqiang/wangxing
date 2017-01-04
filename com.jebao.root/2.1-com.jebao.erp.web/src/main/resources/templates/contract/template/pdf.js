31b49eb86c28177b949582ae78d084927d34e659
yaozd <2674290996@qq.com>
2017-01-04 18:02:27 +0800

    调整生成PDF的js文件

--- com.jebao.root/2.1-com.jebao.erp.web/src/main/resources/templates/contract/template/pdf.js
+++ com.jebao.root/2.1-com.jebao.erp.web/src/main/resources/templates/contract/template/pdf.js
@@ -3,35 +3,26 @@ var address = system.args[1];
 var output = system.args[2];
 var page = require('webpage').create();
 page.paperSize = {
-  format: 'A4',
-  orientation: 'portrait',
-   border: '1cm'
+    format: 'A4',
+    orientation: 'portrait',
+    border: '1cm'
 };
 // debug begin
-// console.log(output);
-// console.log(address);
-// console.log(status);
+//console.log(output);
+//console.log(address);
+//console.log(status);
 // debug end
-page.settings.resourceTimeout = 2000; 
+page.settings.resourceTimeout = 14000;
 page.open(address, function (status) {
     if (status !== 'success') {
         console.log('[ERROR]');
         phantom.exit();
-    } else {
-	console.log("[OK]");
-        window.setTimeout(function () {
-            // Remove all low-opacity paths. see PhantomJS  issue #364 
-            page.evaluate(function () {
-                var paths = document.getElementsByTagName("path");
-                for (var i = paths.length - 1; i >= 0; i--) {
-                    var path = paths[i];
-                    var strokeOpacity = path.getAttribute('stroke-opacity');
-                    if (strokeOpacity != null && strokeOpacity < 0.2)
-                        path.parentNode.removeChild(path);
-                }
-            });
-            page.render(output);
-            phantom.exit();
-        }, 100);
-    }
-});
+    }});
+page.onLoadFinished = function (status) {
+    setTimeout(function () {
+        page.render(output);
+        //console.log('Status: ' + status);
+        console.log("[OK]");
+        phantom.exit();
+    }, 0);
+};
