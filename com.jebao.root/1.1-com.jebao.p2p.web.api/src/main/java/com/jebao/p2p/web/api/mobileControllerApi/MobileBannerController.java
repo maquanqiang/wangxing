fef4644eed362df43c03e9ee674955ef60a7bb94
xiazhen <675785015@qq.com>
2017-02-17 15:47:19 +0800

    修改手banner扩展名

--- com.jebao.root/1.1-com.jebao.p2p.web.api/src/main/java/com/jebao/p2p/web/api/mobileControllerApi/MobileBannerController.java
+++ com.jebao.root/1.1-com.jebao.p2p.web.api/src/main/java/com/jebao/p2p/web/api/mobileControllerApi/MobileBannerController.java
@@ -20,9 +20,9 @@ public class MobileBannerController {
     @RequestMapping("bannerList")
     public JsonResult bannerList(){
         List<MobileBannerVm> bannerList = new ArrayList<>();
-        bannerList.add(new MobileBannerVm(1, "html/phone/images/mobile-banner1.jpg", "html/phone/security.html"));
-        bannerList.add(new MobileBannerVm(2, "html/phone/images/mobile-banner2.jpg", "html/phone/rose.html"));
-        bannerList.add(new MobileBannerVm(3, "html/phone/images/mobile-banner3.jpg", "html/phone/NewWebsite.html"));
+        bannerList.add(new MobileBannerVm(1, "html/phone/images/banner01.png", "html/phone/security.html"));
+        bannerList.add(new MobileBannerVm(2, "html/phone/images/banner02.png", "html/phone/rose.html"));
+        bannerList.add(new MobileBannerVm(3, "html/phone/images/banner03.png", "html/phone/NewWebsite.html"));
 
         return new JsonResultList<>(bannerList);
     }
