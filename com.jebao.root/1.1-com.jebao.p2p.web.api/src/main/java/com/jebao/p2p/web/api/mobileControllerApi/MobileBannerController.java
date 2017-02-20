c7a39b53ba0c021a9c84f56fba0334e0e6d1f5bb
xiazhen <675785015@qq.com>
2017-02-20 10:30:49 +0800

    添加  移动端banner根路径

--- com.jebao.root/1.1-com.jebao.p2p.web.api/src/main/java/com/jebao/p2p/web/api/mobileControllerApi/MobileBannerController.java
+++ com.jebao.root/1.1-com.jebao.p2p.web.api/src/main/java/com/jebao/p2p/web/api/mobileControllerApi/MobileBannerController.java
@@ -3,6 +3,7 @@ package com.jebao.p2p.web.api.mobileControllerApi;
 import com.jebao.p2p.web.api.responseModel.base.JsonResult;
 import com.jebao.p2p.web.api.responseModel.base.JsonResultList;
 import com.jebao.p2p.web.api.responseModel.mobilebanner.MobileBannerVm;
+import com.jebao.p2p.web.api.utils.constants.Constants;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +20,16 @@ public class MobileBannerController {
 
     @RequestMapping("bannerList")
     public JsonResult bannerList(){
+
+
         List<MobileBannerVm> bannerList = new ArrayList<>();
-        bannerList.add(new MobileBannerVm(1, "html/phone/images/banner01.png", "html/phone/security.html"));
-        bannerList.add(new MobileBannerVm(2, "html/phone/images/banner02.png", "html/phone/rose.html"));
-        bannerList.add(new MobileBannerVm(3, "html/phone/images/banner03.png", "html/phone/NewWebsite.html"));
+        String path = "http://192.168.1.7/";
+        if(!Constants.IsTest){
+            path = "http://static1.jebao.net/";
+        }
+        bannerList.add(new MobileBannerVm(1, path+"html/phone/images/banner01.png", path+"html/phone/security.html"));
+        bannerList.add(new MobileBannerVm(2, path+"html/phone/images/banner02.png", path+"html/phone/rose.html"));
+        bannerList.add(new MobileBannerVm(3, path+"html/phone/images/banner03.png", path+"html/phone/NewWebsite.html"));
 
         return new JsonResultList<>(bannerList);
     }
